128f31ba5cb2a0ec0b35bafe3ffd2cea13be2764
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-06-23 19:17:12 -0300

    Exercise 1

---
+++ exercises/fundamentos/bloco_04/dia-1-introducao-javascript/exercice-01-1.js
@@ -0,0 +1,12 @@
+//Requisito 1
+
+//Faça um programa que retorne o maior de dois números. Defina, no começo do programa, duas variáveis com os valores que serão comparados.
+
+let a = 5;
+let b = 10;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
