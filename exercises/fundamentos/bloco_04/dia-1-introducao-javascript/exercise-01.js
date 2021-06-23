dc87ef89c86012fe2ed3f5343e850e372c1d968b
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-06-23 19:26:41 -0300

    Exercicio 3

--- exercises/fundamentos/bloco_04/dia-1-introducao-javascript/exercise-01.js
+++ exercises/fundamentos/bloco_04/dia-1-introducao-javascript/exercise-01.js
@@ -5,8 +5,15 @@
 let a = 5;
 let b = 10;
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
+let soma = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
