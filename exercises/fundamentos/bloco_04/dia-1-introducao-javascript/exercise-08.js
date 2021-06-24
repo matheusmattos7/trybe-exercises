6da6229d674601ec4fe43ff731e9fbbe2146770e
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-06-23 22:13:26 -0300

    Resoluçao exercicio 8

---
+++ exercises/fundamentos/bloco_04/dia-1-introducao-javascript/exercise-08.js
@@ -0,0 +1,12 @@
+// Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+// Bonus: use somente um if .
+
+let x = 1;
+let y = 2;
+let z = 5;
+
+if (x % 2 === 0 || y % 2 === 0 || z % 2 === 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
