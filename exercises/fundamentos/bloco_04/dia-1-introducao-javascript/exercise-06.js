03c5d1e8510492382769cc0fbabe79b73a55e3ec
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-06-23 21:38:50 -0300

    Resoluçao exercicio 6

---
+++ exercises/fundamentos/bloco_04/dia-1-introducao-javascript/exercise-06.js
@@ -0,0 +1,37 @@
+//Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+
+let peças = "rainha";
+
+switch (peças) {
+  case "peao":
+    console.log("movimenta-se uma casa para frente e captura na diagonal");
+      break;
+
+  case "torre":
+    console.log("movimenta-se nos pontos cardinais");
+      break;
+
+
+  case "cavalo":
+    console.log("Se movimenta em L");
+      break;
+
+
+  case "bispo":
+    console.log("Se movimenta em diagonais");
+      break;
+
+
+  case "rainha":
+    console.log("Se movimenta tanto nos ponto cardinais como nos diagonais");
+      break;
+
+  case "rei":
+    console.log("Se movimenta nos pontos cardinais");
+      break;
+
+  default:
+    console.log("Coloque uma peça de xadrez válida");
+} 
+
+
