1f3431d72033d0ec1936ad5e6f8ad88c75419a25
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-06-23 21:05:24 -0300

    Resoluçao exercicio 4

--- exercises/fundamentos/bloco_04/dia-1-introducao-javascript/exercise-01.js
+++ exercises/fundamentos/bloco_04/dia-1-introducao-javascript/exercise-01.js
@@ -1,5 +1,3 @@
-//Requisito 1
-
 //Faça um programa que retorne o maior de dois números. Defina, no começo do programa, duas variáveis com os valores que serão comparados.
 
 let a = 5;
---
+++ exercises/fundamentos/bloco_04/dia-1-introducao-javascript/exercise-03.js
@@ -0,0 +1,13 @@
+//Faça um programa que retorne o maior de três números. Defina, no começo do programa, três variáveis com os valores que serão comparados.
+
+let a = 10
+let b = 15
+let c = 25
+
+if(a > b) {
+  console.log(a);
+}else if (b > c) {
+  console.log (b)
+} else {
+  console.log(c)
+}
