ab5620d8f2869a1b222ebfd8ef9193de6229d140
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-06-24 21:25:42 -0300

    Resoluçao exercise 1

--- exercises/fundamentos/bloco_04/dia-2-javascript-array-e-loop-for/exercise-1.js
+++ exercises/fundamentos/bloco_04/dia-2-javascript-array-e-loop-for/exercise-1.js
@@ -3,5 +3,7 @@ let number = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //Exercicio 1
 //Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 
-console.log(number);
+for (let index = 0; index < number.length; index += 1) {
+    console.log(number[index]);
+}
 
