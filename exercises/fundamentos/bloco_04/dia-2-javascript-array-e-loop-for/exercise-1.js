a0db65d277883de26fb1c5d42eba680f16a911a1
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-06-24 21:29:11 -0300

    Resoluçao exercise 2

--- exercises/fundamentos/bloco_04/dia-2-javascript-array-e-loop-for/exercise-1.js
+++ exercises/fundamentos/bloco_04/dia-2-javascript-array-e-loop-for/exercise-1.js
@@ -1,9 +1,8 @@
-let number = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
 //Exercicio 1
 //Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 
-for (let index = 0; index < number.length; index += 1) {
-    console.log(number[index]);
-}
+let number = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+for (let index = 0; index < number.length; index += 1) {
+  console.log(number[index]);
+}
--- exercises/fundamentos/bloco_04/dia-2-javascript-array-e-loop-for/exercise-2.js
+++ exercises/fundamentos/bloco_04/dia-2-javascript-array-e-loop-for/exercise-2.js
@@ -1,8 +1,11 @@
 //Exercicio 2
 //Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 
-let soma = []
+let number = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
 
-for (index = 0; index < number.length; index +=1 ) {
-    console.log([index])
+for (let index = 0; index < number.length; index += 1) {
+  result += number[index];
 }
+
+console.log(result);
