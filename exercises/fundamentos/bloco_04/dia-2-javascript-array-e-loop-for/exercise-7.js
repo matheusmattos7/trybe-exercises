2893aad181d676bde0434dbb22cb4bfaaa8c09fe
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-06-24 23:00:56 -0300

    Resoluçao exercise 8

--- exercises/fundamentos/bloco_04/dia-2-javascript-array-e-loop-for/exercise-7.js
+++ exercises/fundamentos/bloco_04/dia-2-javascript-array-e-loop-for/exercise-7.js
@@ -4,10 +4,10 @@
 let number = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let lowerValue = number[0];
 
-for (index = 1; index < number.length; index +=1) {
-    if (number[index] < lowerValue) {
-        lowerValue = number[index]
-    }
+for (index = 1; index < number.length; index += 1) {
+  if (number[index] < lowerValue) {
+    lowerValue = number[index]
+  }
 }
 
 console.log(lowerValue);
---
+++ exercises/fundamentos/bloco_04/dia-2-javascript-array-e-loop-for/exercise-8.js
@@ -0,0 +1,10 @@
+//Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+//Exercicio 8
+
+let numbers = [];
+
+for (index = 1; index <= 25 ;index +=1 ){
+  numbers.push(index) ;
+}
+
+console.log(numbers);
